4e1af41f7366895a6dfa01566162b0c145b9189c
cafel176 <48304406+cafel176@users.noreply.github.com>
2021-03-27 18:26:45 +0000

    Add files via upload

--- web/src/main/java/com/ttds/cw3/Data/ResponseData.java
+++ web/src/main/java/com/ttds/cw3/Data/ResponseData.java
@@ -6,10 +6,13 @@ public class ResponseData<T extends Object>
 {
     private String docid;
     private String docname;
+    private String author;
+    private String category;
     private String desc;
+    private String text;
     private T data;
 
-    public ResponseData(String docid, String docname, T data, String desc) {
+    public ResponseData(String docid, String docname,String author, String category, String desc, String text, T data) {
         this.docid = docid;
         if(docname==null||docname.isEmpty())
             this.docname = docid;
@@ -17,6 +20,9 @@ public class ResponseData<T extends Object>
             this.docname = docname;
         this.data = data;
         this.desc = desc;
+        this.author = author;
+        this.category = category;
+        this.text = text;
     }
 
     public ResponseData(SearchResultAdapter<T> t)
@@ -29,14 +35,17 @@ public class ResponseData<T extends Object>
             this.docname = docname;
         data = t.getValue();
         desc = t.getDesc();
-    }
-
-    public void setDocid(String docid) {
-        this.docid = docid;
+        author = t.getAuthor();
+        category = t.getCategory();
+        text = t.getText();
     }
 
     public String getDocid() {
         return docid;
+    }
+
+    public void setDocid(String docid) {
+        this.docid = docid;
     }
 
     public String getDocname() {
@@ -47,12 +56,20 @@ public class ResponseData<T extends Object>
         this.docname = docname;
     }
 
-    public T getData() {
-        return data;
+    public String getAuthor() {
+        return author;
     }
 
-    public void setData(T data) {
-        this.data = data;
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     public String getDesc() {
@@ -61,5 +78,21 @@ public class ResponseData<T extends Object>
 
     public void setDesc(String desc) {
         this.desc = desc;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
     }
 }
