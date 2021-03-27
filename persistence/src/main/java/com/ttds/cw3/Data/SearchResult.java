aeba16b3b967131ccf2cd8fc4a854bd9dfc0b4f8
cafel176 <48304406+cafel176@users.noreply.github.com>
2021-03-27 18:28:35 +0000

    Add files via upload

--- persistence/src/main/java/com/ttds/cw3/Data/SearchResult.java
+++ persistence/src/main/java/com/ttds/cw3/Data/SearchResult.java
@@ -6,13 +6,18 @@ public class SearchResult<T extends Object> implements SearchResultInterface
 {
     private String docid;
     private String docname;
+    private String author;
+    private String category;
     private String desc;
+    private String text;
     private T value;
 
-    public SearchResult(String docid, String docname, T value) {
+    public SearchResult(String docid, String docname,String author,String category, T value) {
         this.docid = docid;
         this.docname = docname;
         this.value = value;
+        this.author = author;
+        this.category = category;
     }
 
     public SearchResult(SearchResult other) {
@@ -20,6 +25,9 @@ public class SearchResult<T extends Object> implements SearchResultInterface
         this.docname = other.getDocName();
         this.value = (T)other.getValue();
         this.desc = other.getDesc();
+        this.author = other.author;
+        this.category = other.category;
+        this.text = other.text;
     }
 
     public String getDocid() {
@@ -40,5 +48,29 @@ public class SearchResult<T extends Object> implements SearchResultInterface
 
     public String getDesc() {
         return desc;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 }
