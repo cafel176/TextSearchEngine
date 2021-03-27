d00f0b8894e36d86baafa040d7702cd5e78f8dd1
cafel176 <48304406+cafel176@users.noreply.github.com>
2021-03-27 18:24:55 +0000

    Add files via upload

---
+++ persistence/src/main/java/com/ttds/cw3/Adapter/SearchResultAdapter.java
@@ -0,0 +1,48 @@
+package com.ttds.cw3.Adapter;
+
+import com.ttds.cw3.Interface.SearchResultInterface;
+
+public class SearchResultAdapter<T>
+{
+    private SearchResultInterface<T> result;
+
+    public SearchResultAdapter(SearchResultInterface<T> result) {
+        this.result = result;
+    }
+
+    public void setResult(SearchResultInterface<T> result) {
+        this.result = result;
+    }
+
+    public String getDocid()
+    {
+        return result.getDocid();
+    }
+
+    public String getDocName()
+    {
+        return result.getDocName();
+    }
+
+    public T getValue()
+    {
+        return result.getValue();
+    }
+
+    public String getDesc()
+    {
+        return result.getDesc();
+    }
+
+    public String getText() {
+        return result.getText();
+    }
+
+    public String getAuthor() {
+        return result.getAuthor();
+    }
+
+    public String getCategory() {
+        return result.getCategory();
+    }
+}
